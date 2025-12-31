1faad0b0841932caf3ed4fad0073e43a198d31d4
pallapusivasai <pallapu.sivasai.33@gmail.com>
2025-12-31 12:06:09 +0530

    Added sql challenges

---
+++ 09-mysql/challenges/department-stats.sql
@@ -0,0 +1,9 @@
+SELECT 
+    department,
+    COUNT(*) AS employee_count,
+    ROUND(AVG(salary), 2) AS avg_salary,
+    MAX(salary) AS max_salary
+FROM employees
+GROUP BY department
+HAVING COUNT(*) > 2;
+-- This query retrieves the number of employees, average salary, and maximum salary for each department
